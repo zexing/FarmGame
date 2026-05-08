9d230fe4b0cf8f5879f977eca56e204c2f39e6fc
zexing24 <zexing24@foxmail.com>
2026-05-08 15:57:12 +0800

    1

---
+++ assets/scripts/game/role/RoleAnimation.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('RoleAnimation')
+export class RoleAnimation extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
